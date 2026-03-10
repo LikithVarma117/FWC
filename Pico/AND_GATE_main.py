7023caa8c1fc18cffbb51dec3201b64e475af647
Likith Varma <likithvarma117@gmail.com>
2026-03-10 16:09:05 +0530

    Add AND gate simulation with 7-segment display
    Implement 7-segment display for AND gate simulation.

---
+++ Pico/AND_GATE_main.py
@@ -0,0 +1,56 @@
+from machine import Pin
+import time
+
+# 7-segment pins
+a = Pin(2, Pin.OUT)
+b = Pin(3, Pin.OUT)
+c = Pin(4, Pin.OUT)
+d = Pin(5, Pin.OUT)
+e = Pin(6, Pin.OUT)
+f = Pin(7, Pin.OUT)
+g = Pin(8, Pin.OUT)
+
+# -------- Display Functions --------
+
+def show0():
+    a.value(0)
+    b.value(0)
+    c.value(0)
+    d.value(0)
+    e.value(0)
+    f.value(0)
+    g.value(1)
+
+def show1():
+    a.value(1)
+    b.value(0)
+    c.value(0)
+    d.value(1)
+    e.value(1)
+    f.value(1)
+    g.value(1)
+
+# -------- Test Inputs --------
+# A and B combinations
+
+inputs = [(0,0),(0,1),(1,0),(1,1)]
+
+while True:
+    
+    for A, B in inputs:
+
+        # MUX inputs
+        I0 = 0
+        I1 = B
+
+        # MUX equation
+        Y = ((not A) and I0) or (A and I1)
+
+        if Y:
+            show1()
+        else:
+            show0()
+
+        print("A =",A," B =",B," Output =",Y)
+
+        time.sleep(2)
